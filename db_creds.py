e36e5bbb304d5caef0631e0855443b46f5da6bdc
Emiel Demeestere <emiel.demeestere@ugent.be>
2023-04-26 00:41:02 +0200

    Make database credentials settable via env vars
    Signed-off-by: Emiel Demeestere <emiel.demeestere@ugent.be>

--- db_creds.py
+++ db_creds.py
@@ -1,5 +1,7 @@
-DB_USER     = "postgres"
-DB_PASSWORD = "postgres"
-DB_HOST     = "localhost"
-DB_PORT     = "5432"
-DB_NAME     = "nmea_data"
+import os
+
+DB_USER     = os.environ.get('DB_USER') if 'DB_USER' in os.environ else 'postgres'
+DB_PASSWORD = os.environ.get('DB_PASSWORD') if 'DB_PASSWORD' in os.environ else 'postgres'
+DB_HOST     = os.environ.get('DB_HOST') if 'DB_HOST' in os.environ else 'localhost'
+DB_PORT     = os.environ.get('DB_PORT') if 'DB_PORT' in os.environ else '5432'
+DB_NAME     = os.environ.get('DB_NAME') if 'DB_NAME' in os.environ else 'nmea_data'
